b93d7eb3b08f5d0d73ab1851c0282117bed41771
BagusRezky <bagusrezky15@gmail.com>
2024-06-27 22:59:09 +0700

    chore: Update BillboardDetail component to use new MQTT endpoint and fix interaction count display

--- webservice/src/pages/BillboardDetail.jsx
+++ webservice/src/pages/BillboardDetail.jsx
@@ -1,114 +1,3 @@
-// import { useEffect, useRef } from "react";
-// import { useParams } from "react-router-dom";
-// import videojs from "video.js";
-// import "videojs-contrib-hls";
-// // import "video.js/dist/video-js.css";
-// // import mqtt from "mqtt";
-
-// function BillboardDetail() {
-//   const { id } = useParams();
-//   const videoRef = useRef(null);
-  
-//   // const [interactions, setInteractions] = useState({ down: 0, up: 0 });
-
-//   // useEffect(() => {
-//   //   const client = mqtt.connect("ws://localhost:9001/");
-
-//   //   client.on("connect", () => {
-//   //     console.log("Connected to MQTT broker");
-//   //     client.subscribe("vehicle/interactions", (err) => {
-//   //       if (!err) {
-//   //         console.log("Subscribed to vehicle/interactions");
-//   //       }
-//   //     });
-//   //   });
-
-//   //   client.on("message", (topic, message) => {
-//   //     if (topic === "vehicle/interactions") {
-//   //       const data = JSON.parse(message.toString());
-//   //       console.log("Received MQTT message:", data);
-//   //       setInteractions(data);
-//   //     }
-//   //   });
-
-//   //   client.on("error", (err) => {
-//   //     console.error("MQTT Connection Error: ", err);
-//   //   });
-
-//   //   return () => {
-//   //     client.end();
-//   //   };
-//   // }, []);
-
-//   // useEffect(() => {
-//   //   console.log("Initializing Video.js player");
-//   //   const player = videojs(document.getElementById("rtmp-video"), {
-//   //     autoplay: true,
-//   //     controls: true,
-//   //     techOrder: ["flash", "html5"],
-//   //     sources: [
-//   //       {
-//   //         // src: "rtmp://localhost:1935/live/mystream",
-//   //         src: "rtmp://localhost/live/test",
-//   //         type: "rtmp/flv",
-//   //       },
-//   //     ],
-//   //   });
-
-//   //   player.on("ready", () => {
-//   //     console.log("Video.js player is ready");
-//   //   });
-
-//   //   player.on("error", (e) => {
-//   //     console.log("Error with video.js player:", e);
-//   //   });
-
-//   //   return () => {
-//   //     if (player) {
-//   //       player.dispose();
-//   //     }
-//   //   };
-//   // }, []);
-
-//   const billboard = {
-//     id,
-//     size: "4x8",
-//     address: "Jl. Raya Bogor, No. 5, Jakarta",
-//     date: "Kamis, 7 Februari 2024",
-//     time: "13.40",
-//     location: "Jl. Soekarno Hatta",
-//   };
-
-//   return (
-//     <div>
-//       <h2 className="text-2xl font-bold mb-4">Billboard Detail</h2>
-//       <div className="flex flex-wrap justify-around items-center mt-2">
-//         <video
-//           ref={videoRef}
-//           id="rtmp-video"
-//           className="video-js vjs-default-skin"
-//           width="850"
-//           height="480"
-//           controls
-//         ></video>
-//         <div className="bg-gray-100 p-9 rounded">
-//           <p className="text-md mb-2">{`Hari/Tanggal: ${billboard.date}`}</p>
-//           <p className="text-md mb-2">{`Lokasi: ${billboard.location}`}</p>
-//           <p className="text-md mb-2">{`Waktu: ${billboard.time}`}</p>
-//           <div className="mt-10 text-center">
-//             <p className="text-2xl font-bold">Jumlah Interaksi</p>
-//             {/* <p className="text-5xl font-bold">{`Down: ${interactions.down}`}</p>
-//             <p className="text-5xl font-bold">{`Up: ${interactions.up}`}</p> */}
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default BillboardDetail;
-
-
 import { useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
@@ -122,7 +11,10 @@ function BillboardDetail() {
   const videoRef = useRef(null);
   const [isReady, setIsReady] = useState(false);
   const [videoUrl, setVideoUrl] = useState("");
-  const [interactions, setInteractions] = useState({ down: 0, up: 0 });
+  const [interactions, setInteractions] = useState({
+    going_down: 0,
+    going_up: 0,
+  });
 
   useEffect(() => {
     // Membuat permintaan Axios untuk memastikan CORS
@@ -194,7 +86,7 @@ function BillboardDetail() {
 
 
   useEffect(() => {
-    const client = mqtt.connect("ws://localhost:9001");
+    const client = mqtt.connect("ws://localhost:9001/mqtt");
 
     client.on("connect", () => {
       console.log("Connected to MQTT broker");
@@ -252,8 +144,8 @@ function BillboardDetail() {
             <p className="text-md mb-2">{`Waktu: ${billboard.time}`}</p>
             <div className="mt-10 text-center">
               <p className="text-2xl font-bold">Jumlah Interaksi</p>
-              <p className="text-5xl font-bold">{`Down: ${interactions.down}`}</p>
-              <p className="text-5xl font-bold">{`Up: ${interactions.up}`}</p>
+              <p className="text-5xl font-bold">{`Down: ${interactions.going_down}`}</p>
+              <p className="text-5xl font-bold">{`Up: ${interactions.going_up}`}</p>
             </div>
           </div>
         )}
