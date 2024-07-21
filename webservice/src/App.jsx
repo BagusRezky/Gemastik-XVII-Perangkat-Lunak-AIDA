26b5772e185ddf216f2ff0a261332199b8e1ea6b
BagusRezky <bagusrezky15@gmail.com>
2024-07-21 17:42:32 +0700

    chore: Remove commented out code for AllBillboard component in App.jsx

--- webservice/src/App.jsx
+++ webservice/src/App.jsx
@@ -2,7 +2,6 @@ import DashboardContainer from "./pages/DashboardContainer";
 import AllBillboard from "./pages/AllBilboard";
 import ReportList from "./pages/ReportList";
 import BillboardReport from "./pages/BillboardReport";
-// import AllBillboard from "./pages/AllBillboard";
 import './index.css'
 import Sidebar from "./components/SideBar";
 import { Routes, Route } from "react-router-dom";
