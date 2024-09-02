6fe105f8c2e2383c8d05852757bdb36e5e21802b
BagusRezky <bagusrezky15@gmail.com>
2024-09-02 18:25:37 +0700

    chore: Refactor tracker.py to improve object detection and tracking

--- python/tracker.py
+++ python/tracker.py
@@ -1,49 +1,48 @@
 import math
+
 class Tracker:
     def __init__(self):
-        # Store the center positions of the objects
+        # Menyimpan posisi pusat objek
         self.center_points = {}
-        # Keep the count of the IDs
-        # each time a new object id detected, the count will increase by one
+        # Menyimpan jumlah ID
+        # Setiap kali objek baru terdeteksi, ID bertambah satu
         self.id_count = 0
 
-
     def update(self, objects_rect):
-        # Objects boxes and ids
+        # Kotak objek dan ID
         objects_bbs_ids = []
 
-        # Get center point of new object
+        # Mendapatkan titik pusat objek baru
         for rect in objects_rect:
             x, y, w, h = rect
             cx = (x + x + w) // 2
             cy = (y + y + h) // 2
 
-            # Find out if that object was detected already
+            # Mencari tahu apakah objek tersebut sudah terdeteksi
             same_object_detected = False
             for id, pt in self.center_points.items():
                 dist = math.hypot(cx - pt[0], cy - pt[1])
 
-                if dist < 35:
+                if dist < 35:  # Jika jarak pusatnya kurang dari threshold
                     self.center_points[id] = (cx, cy)
-#                    print(self.center_points)
                     objects_bbs_ids.append([x, y, w, h, id])
                     same_object_detected = True
                     break
 
-            # New object is detected we assign the ID to that object
-            if same_object_detected is False:
+            # Objek baru terdeteksi, berikan ID ke objek tersebut
+            if not same_object_detected:
                 self.center_points[self.id_count] = (cx, cy)
                 objects_bbs_ids.append([x, y, w, h, self.id_count])
                 self.id_count += 1
 
-        # Clean the dictionary by center points to remove IDS not used anymore
+        # Membersihkan dictionary dengan menghapus ID yang tidak digunakan lagi
         new_center_points = {}
         for obj_bb_id in objects_bbs_ids:
             _, _, _, _, object_id = obj_bb_id
             center = self.center_points[object_id]
             new_center_points[object_id] = center
 
-        # Update dictionary with IDs not used removed
+        # Memperbarui dictionary dengan ID yang tidak digunakan dihapus
         self.center_points = new_center_points.copy()
-        
+
         return objects_bbs_ids
