2cb2741d4167b8f763d1f012512c35ded3375501
BagusRezky <bagusrezky15@gmail.com>
2024-10-03 07:58:42 +0700

    Refactor LineGraph component to fetch and process data from the server

--- frontend/src/components/LineGraph.jsx
+++ frontend/src/components/LineGraph.jsx
@@ -1,4 +1,4 @@
-
+import { useState, useEffect } from "react";
 import {
   LineChart,
   Line,
@@ -8,23 +8,56 @@ import {
   Tooltip,
 } from "recharts";
 
-const data = [
-  { name: "Jan", value: 400 },
-  { name: "Feb", value: 300 },
-  { name: "Mar", value: 500 },
-  { name: "Apr", value: 200 },
-  { name: "May", value: 300 },
-  { name: "Jun", value: 400 },
-];
-
 function LineGraph() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, );
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        "http://localhost:3000/interactions/BillboardA"
+      );
+      const jsonData = await response.json();
+      const processedData = processData(jsonData);
+      setData(processedData);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  const processData = (rawData) => {
+    const monthlyData = rawData.reduce((acc, item) => {
+      const date = new Date(item.timestamp);
+      const monthYear = `${date.getFullYear()}-${String(
+        date.getMonth() + 1
+      ).padStart(2, "0")}`;
+
+      if (!acc[monthYear]) {
+        acc[monthYear] = { going_down: 0, going_up: 0 };
+      }
+
+      acc[monthYear].going_down += item.going_down;
+      acc[monthYear].going_up += item.going_up;
+
+      return acc;
+    }, {});
+
+    return Object.entries(monthlyData).map(([date, values]) => ({
+      name: date,
+      total: values.going_down + values.going_up,
+    }));
+  };
+
   return (
     <div className="bg-white shadow rounded-lg p-4">
       <h4 className="font-bold text-gray-600">
         Total Interaksi yang Melewati Billboard per Bulan
       </h4>
-      <LineChart width={1100} height={300} data={data}>
-        <Line type="monotone" dataKey="value" stroke="#8884d8" />
+      <LineChart width={1400} height={300} data={data}>
+        <Line type="monotone" dataKey="total" stroke="#8884d8" />
         <CartesianGrid stroke="#ccc" />
         <XAxis dataKey="name" />
         <YAxis />
